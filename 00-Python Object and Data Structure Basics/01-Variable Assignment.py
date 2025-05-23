377265888a40722e15ffcbb4b4095598984b191e
Syeda Zunehra Banu <118747783+zunehraBanu@users.noreply.github.com>
2025-05-22 22:39:49 -0500

    Variable Assignment

---
+++ 00-Python Object and Data Structure Basics/01-Variable Assignment.py
@@ -0,0 +1,41 @@
+# Variable Assignment in Python
+
+# Dynamic Typing Example
+my_dogs = 2
+print(my_dogs)
+
+my_dogs = ['Sammy', 'Frankie']
+print(my_dogs)
+
+# Assigning Variables
+a = 5
+print(a)
+
+a = 10
+print(a)
+
+# Using variables in expressions
+print(a + a)
+
+# Reassigning variables
+a = a + 10
+print(a)
+
+# Shortcut assignment operators
+a += 10
+print(a)
+
+a *= 2
+print(a)
+
+# Determining variable type
+print(type(a))
+
+a = (1, 2)
+print(type(a))
+
+# Simple Exercise
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
