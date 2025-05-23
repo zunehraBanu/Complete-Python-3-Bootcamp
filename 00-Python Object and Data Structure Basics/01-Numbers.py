6f2a0357d3705daebd016fe637b51da850f7f011
Syeda Zunehra Banu <118747783+zunehraBanu@users.noreply.github.com>
2025-05-22 22:34:28 -0500

    Create 01-Numbers.py

---
+++ 00-Python Object and Data Structure Basics/01-Numbers.py
@@ -0,0 +1,60 @@
+# Addition
+2+1
+
+# Subtraction
+2-1
+
+# Multiplication
+2*2
+
+# Division
+3/2
+
+# Floor Division
+7//4
+
+# Modulo
+7%4
+
+# Powers
+2**3
+
+# Can also do roots this way
+4**0.5
+
+# Order of Operations followed in Python
+2 + 10 * 10 + 3
+
+# Can use parentheses to specify orders
+(2+10) * (10+3)
+
+# Let's create an object called "a" and assign it the number 5
+a = 5
+
+# Adding the objects
+a+a
+
+# Reassignment
+a = 10
+
+# Check
+a
+
+# Check
+a
+
+# Use A to redefine A
+a = a + a
+
+# Check 
+a
+
+# Use object names to keep better track of what's going on in your code!
+my_income = 100
+
+tax_rate = 0.1
+
+my_taxes = my_income * tax_rate
+
+# Show my taxes!
+my_taxes
